3fe64a9ef3a4d1922c66c780bc2b404e56772545
Smachoka <silvanusmachoka@gmail.com>
2024-07-20 08:21:06 +0300

    list

---
+++ List/indexing.py
@@ -0,0 +1,6 @@
+fruit = ["banana",  "cherry", "kiwi", "mango"]
+print(fruit[1]) # accessing list
+print(fruit[-1])
+print(fruit[1:3]) # slicing list
+fruit.append("oranges") # adding elements
+print(fruit)    # printing list
