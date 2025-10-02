4ca0bc764ede6c27234a3501437aeac65874a672
Smachoka <silvanusmachoka@gmail.com>
2025-10-02 14:23:04 +0300

    added strings

---
+++ strings/slicing.py
@@ -0,0 +1,14 @@
+text ="hello world"
+age = 30
+print(text[2:5])
+print(text[:5])
+print(text[2:])
+print(text[-5:-2])
+print(text[-5:])
+print(text[-5:])
+print(text[:])
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.title())
+print(f" I am {age} years old")
