585f04b67aaf6b12cd1b4049d96133c007926649
Smachoka <silvanusmachoka@gmail.com>
2025-10-02 12:07:21 +0300

    initial commit

---
+++ strings/app.py
@@ -0,0 +1,8 @@
+text= " I love my python language"
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.strip())
+print("python" in text)
+print("python" not in text)
+print(len(text))
