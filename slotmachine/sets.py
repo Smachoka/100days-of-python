99d782e7cb078f89a2d433a97ca403d981d52af0
Smachoka <silvanusmachoka@gmail.com>
2025-10-06 13:13:40 +0300

    added dictionaries

---
+++ slotmachine/sets.py
@@ -0,0 +1,19 @@
+fruits ={"cherry", "lemon", "orange", "plum", "bell", "bar", "seven"}
+fruit_list= {"cherry","banana","apple","grape","orange","kiwi","mango","peach","pear","watermelon"}
+combine = fruits - fruit_list
+combine = fruits - fruit_list
+print(combine)
+combine = fruits | fruit_list
+print(combine)
+combine = fruits & fruit_list
+print(combine)
+combine = fruits <= fruit_list
+print(combine)
+combine = fruits >= fruit_list
+print(combine)
+combine = fruits < fruit_list
+print(combine)
+combine = fruits > fruit_list
+print(combine)
+combine = fruits ^  fruit_list
+print(combine)
